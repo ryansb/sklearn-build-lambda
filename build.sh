ae49f7f2813a995e3b90e4dab23b785b4e891e88
Ryan S. Brown <sb@ryansb.com>
2017-11-14 17:29:31 -0500

    Test Python3.6 support

--- build.sh
+++ build.sh
@@ -12,21 +12,31 @@ yum install -y \
     python27-devel \
     python27-virtualenv \
     findutils \
+    xz \
     zip
 
+rm -rf /var/runtime /var/lang && \
+  curl https://lambci.s3.amazonaws.com/fs/python3.6.tgz | tar -xz -C / && \
+  curl https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz | tar -xJ && \
+  cd Python-3.6.1 && \
+  ./configure --prefix=/var/lang && \
+  make -j$(getconf _NPROCESSORS_ONLN) libinstall inclinstall && \
+  cd .. && \
+  rm -rf Python-3.6.1
+
 do_pip () {
-    pip install --upgrade pip wheel
-    pip install --use-wheel --no-binary numpy numpy
-    pip install --use-wheel --no-binary scipy scipy
-    pip install --use-wheel sklearn
+    #pip install --upgrade pip wheel
+    /var/lang/bin/pip3 install --use-wheel --no-binary numpy numpy
+    /var/lang/bin/pip3 install --use-wheel --no-binary scipy scipy
+    /var/lang/bin/pip3 install --use-wheel sklearn
 }
 
 strip_virtualenv () {
     echo "venv original size $(du -sh $VIRTUAL_ENV | cut -f1)"
-    find $VIRTUAL_ENV/lib64/python2.7/site-packages/ -name "*.so" | xargs strip
+    find $VIRTUAL_ENV/lib64/python3.6/site-packages/ -name "*.so" | xargs strip
     echo "venv stripped size $(du -sh $VIRTUAL_ENV | cut -f1)"
 
-    pushd $VIRTUAL_ENV/lib64/python2.7/site-packages/ && zip -r -9 -q /outputs/venv.zip * ; popd
+    pushd $VIRTUAL_ENV/lib64/python3.6/site-packages/ && zip -r -9 -q /outputs/venv.zip * ; popd
     echo "site-packages compressed size $(du -sh /outputs/venv.zip | cut -f1)"
 
     pushd $VIRTUAL_ENV && zip -r -q /outputs/full-venv.zip * ; popd
@@ -34,8 +44,8 @@ strip_virtualenv () {
 }
 
 shared_libs () {
-    libdir="$VIRTUAL_ENV/lib64/python2.7/site-packages/lib/"
-    mkdir -p $VIRTUAL_ENV/lib64/python2.7/site-packages/lib || true
+    libdir="$VIRTUAL_ENV/lib64/python3.6/site-packages/lib/"
+    mkdir -p $VIRTUAL_ENV/lib64/python3.6/site-packages/lib || true
     cp /usr/lib64/atlas/* $libdir
     cp /usr/lib64/libquadmath.so.0 $libdir
     cp /usr/lib64/libgfortran.so.3 $libdir
@@ -43,7 +53,7 @@ shared_libs () {
 
 main () {
     /usr/bin/virtualenv \
-        --python /usr/bin/python /sklearn_build \
+        --python /var/lang/bin/python3 /sklearn_build \
         --always-copy \
         --no-site-packages
     source /sklearn_build/bin/activate
